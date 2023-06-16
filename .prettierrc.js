8e557b84bb1a78d997f722d343b80279a234eb05
Magomed <fayamiyagi1@gmail.com>
2023-06-16 15:58:48 +0300

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 150,
+  trailingComma: "none",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  arrowParens: "always",
+  useTabs: false,
+  plugins: [],
+};
